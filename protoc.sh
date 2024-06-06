a152849442736f30fa3efd6ff3ad595356a6aff7
nileshdt <nileshdt@gmail.com>
2024-06-05 23:16:37 -0400

    proto update

--- protoc.sh
+++ protoc.sh
@@ -5,11 +5,15 @@ RELEASE_VERSION=$2
 USER_NAME=$3
 EMAIL=$4
 
+echo "SERVICE_NAME: $SERVICE_NAME"
+echo "RELEASE_VERSION: $RELEASE_VERSION"
+echo "USER_NAME: $USER_NAME"
+echo "EMAIL: $EMAIL"
+
 git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
-sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
