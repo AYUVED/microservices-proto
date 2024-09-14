dc49fed81189bdb8ce749411aea09ddf46304773
nileshdt <nileshdt@gmail.com>
2024-09-14 16:59:01 -0400

    proto update

--- golang/log/log_grpc.pb.go
+++ golang/log/log_grpc.pb.go
@@ -19,101 +19,139 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Payment_Create_FullMethodName = "/Payment/Create"
+	Log_Add_FullMethodName = "/Log/Add"
+	Log_Get_FullMethodName = "/Log/Get"
 )
 
-// PaymentClient is the client API for Payment service.
+// LogClient is the client API for Log service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type PaymentClient interface {
-	Create(ctx context.Context, in *CreateLogRequest, opts ...grpc.CallOption) (*CreateLogResponse, error)
+type LogClient interface {
+	Add(ctx context.Context, in *CreateLogRequest, opts ...grpc.CallOption) (*CreateLogResponse, error)
+	Get(ctx context.Context, in *GetLogRequest, opts ...grpc.CallOption) (*GetLogResponse, error)
 }
 
-type paymentClient struct {
+type logClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewPaymentClient(cc grpc.ClientConnInterface) PaymentClient {
-	return &paymentClient{cc}
+func NewLogClient(cc grpc.ClientConnInterface) LogClient {
+	return &logClient{cc}
 }
 
-func (c *paymentClient) Create(ctx context.Context, in *CreateLogRequest, opts ...grpc.CallOption) (*CreateLogResponse, error) {
+func (c *logClient) Add(ctx context.Context, in *CreateLogRequest, opts ...grpc.CallOption) (*CreateLogResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(CreateLogResponse)
-	err := c.cc.Invoke(ctx, Payment_Create_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Log_Add_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// PaymentServer is the server API for Payment service.
-// All implementations must embed UnimplementedPaymentServer
-// for forward compatibility.
-type PaymentServer interface {
-	Create(context.Context, *CreateLogRequest) (*CreateLogResponse, error)
-	mustEmbedUnimplementedPaymentServer()
+func (c *logClient) Get(ctx context.Context, in *GetLogRequest, opts ...grpc.CallOption) (*GetLogResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GetLogResponse)
+	err := c.cc.Invoke(ctx, Log_Get_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
-// UnimplementedPaymentServer must be embedded to have
+// LogServer is the server API for Log service.
+// All implementations must embed UnimplementedLogServer
+// for forward compatibility.
+type LogServer interface {
+	Add(context.Context, *CreateLogRequest) (*CreateLogResponse, error)
+	Get(context.Context, *GetLogRequest) (*GetLogResponse, error)
+	mustEmbedUnimplementedLogServer()
+}
+
+// UnimplementedLogServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedPaymentServer struct{}
+type UnimplementedLogServer struct{}
 
-func (UnimplementedPaymentServer) Create(context.Context, *CreateLogRequest) (*CreateLogResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+func (UnimplementedLogServer) Add(context.Context, *CreateLogRequest) (*CreateLogResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Add not implemented")
 }
-func (UnimplementedPaymentServer) mustEmbedUnimplementedPaymentServer() {}
-func (UnimplementedPaymentServer) testEmbeddedByValue()                 {}
+func (UnimplementedLogServer) Get(context.Context, *GetLogRequest) (*GetLogResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
+}
+func (UnimplementedLogServer) mustEmbedUnimplementedLogServer() {}
+func (UnimplementedLogServer) testEmbeddedByValue()             {}
 
-// UnsafePaymentServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to PaymentServer will
+// UnsafeLogServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to LogServer will
 // result in compilation errors.
-type UnsafePaymentServer interface {
-	mustEmbedUnimplementedPaymentServer()
+type UnsafeLogServer interface {
+	mustEmbedUnimplementedLogServer()
 }
 
-func RegisterPaymentServer(s grpc.ServiceRegistrar, srv PaymentServer) {
-	// If the following call pancis, it indicates UnimplementedPaymentServer was
+func RegisterLogServer(s grpc.ServiceRegistrar, srv LogServer) {
+	// If the following call pancis, it indicates UnimplementedLogServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&Payment_ServiceDesc, srv)
+	s.RegisterService(&Log_ServiceDesc, srv)
 }
 
-func _Payment_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Log_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateLogRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(PaymentServer).Create(ctx, in)
+		return srv.(LogServer).Add(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Payment_Create_FullMethodName,
+		FullMethod: Log_Add_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PaymentServer).Create(ctx, req.(*CreateLogRequest))
+		return srv.(LogServer).Add(ctx, req.(*CreateLogRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Payment_ServiceDesc is the grpc.ServiceDesc for Payment service.
+func _Log_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetLogRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(LogServer).Get(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Log_Get_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(LogServer).Get(ctx, req.(*GetLogRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Log_ServiceDesc is the grpc.ServiceDesc for Log service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Payment_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "Payment",
-	HandlerType: (*PaymentServer)(nil),
+var Log_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "Log",
+	HandlerType: (*LogServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Create",
-			Handler:    _Payment_Create_Handler,
+			MethodName: "Add",
+			Handler:    _Log_Add_Handler,
+		},
+		{
+			MethodName: "Get",
+			Handler:    _Log_Get_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
