28bb66d7557a952b5204d6a2c159cba9315db7b7
nileshdt <nileshdt@gmail.com>
2024-10-27 21:27:55 -0400

    proto update

--- golang/mailer/mailer_grpc.pb.go
+++ golang/mailer/mailer_grpc.pb.go
@@ -19,18 +19,18 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Log_Add_FullMethodName            = "/Log/Add"
-	Log_Get_FullMethodName            = "/Log/Get"
-	Log_AddTemplate_FullMethodName    = "/Log/AddTemplate"
-	Log_GetTemplate_FullMethodName    = "/Log/GetTemplate"
-	Log_UpdateTemplate_FullMethodName = "/Log/UpdateTemplate"
-	Log_DeleteTemplate_FullMethodName = "/Log/DeleteTemplate"
+	Mailer_Add_FullMethodName            = "/Mailer/Add"
+	Mailer_Get_FullMethodName            = "/Mailer/Get"
+	Mailer_AddTemplate_FullMethodName    = "/Mailer/AddTemplate"
+	Mailer_GetTemplate_FullMethodName    = "/Mailer/GetTemplate"
+	Mailer_UpdateTemplate_FullMethodName = "/Mailer/UpdateTemplate"
+	Mailer_DeleteTemplate_FullMethodName = "/Mailer/DeleteTemplate"
 )
 
-// LogClient is the client API for Log service.
+// MailerClient is the client API for Mailer service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type LogClient interface {
+type MailerClient interface {
 	Add(ctx context.Context, in *CreateMailerRequest, opts ...grpc.CallOption) (*CreateMailerResponse, error)
 	Get(ctx context.Context, in *GetMailerRequest, opts ...grpc.CallOption) (*GetMailerResponse, error)
 	AddTemplate(ctx context.Context, in *CreateTemplateRequest, opts ...grpc.CallOption) (*CreateTemplateResponse, error)
@@ -39,271 +39,271 @@ type LogClient interface {
 	DeleteTemplate(ctx context.Context, in *DeleteTemplateRequest, opts ...grpc.CallOption) (*DeleteTemplateResponse, error)
 }
 
-type logClient struct {
+type mailerClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewLogClient(cc grpc.ClientConnInterface) LogClient {
-	return &logClient{cc}
+func NewMailerClient(cc grpc.ClientConnInterface) MailerClient {
+	return &mailerClient{cc}
 }
 
-func (c *logClient) Add(ctx context.Context, in *CreateMailerRequest, opts ...grpc.CallOption) (*CreateMailerResponse, error) {
+func (c *mailerClient) Add(ctx context.Context, in *CreateMailerRequest, opts ...grpc.CallOption) (*CreateMailerResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(CreateMailerResponse)
-	err := c.cc.Invoke(ctx, Log_Add_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_Add_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logClient) Get(ctx context.Context, in *GetMailerRequest, opts ...grpc.CallOption) (*GetMailerResponse, error) {
+func (c *mailerClient) Get(ctx context.Context, in *GetMailerRequest, opts ...grpc.CallOption) (*GetMailerResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(GetMailerResponse)
-	err := c.cc.Invoke(ctx, Log_Get_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_Get_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logClient) AddTemplate(ctx context.Context, in *CreateTemplateRequest, opts ...grpc.CallOption) (*CreateTemplateResponse, error) {
+func (c *mailerClient) AddTemplate(ctx context.Context, in *CreateTemplateRequest, opts ...grpc.CallOption) (*CreateTemplateResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(CreateTemplateResponse)
-	err := c.cc.Invoke(ctx, Log_AddTemplate_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_AddTemplate_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logClient) GetTemplate(ctx context.Context, in *GetTemplateRequest, opts ...grpc.CallOption) (*GetTemplateResponse, error) {
+func (c *mailerClient) GetTemplate(ctx context.Context, in *GetTemplateRequest, opts ...grpc.CallOption) (*GetTemplateResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(GetTemplateResponse)
-	err := c.cc.Invoke(ctx, Log_GetTemplate_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_GetTemplate_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logClient) UpdateTemplate(ctx context.Context, in *UpdateTemplateRequest, opts ...grpc.CallOption) (*UpdateTemplateResponse, error) {
+func (c *mailerClient) UpdateTemplate(ctx context.Context, in *UpdateTemplateRequest, opts ...grpc.CallOption) (*UpdateTemplateResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(UpdateTemplateResponse)
-	err := c.cc.Invoke(ctx, Log_UpdateTemplate_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_UpdateTemplate_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logClient) DeleteTemplate(ctx context.Context, in *DeleteTemplateRequest, opts ...grpc.CallOption) (*DeleteTemplateResponse, error) {
+func (c *mailerClient) DeleteTemplate(ctx context.Context, in *DeleteTemplateRequest, opts ...grpc.CallOption) (*DeleteTemplateResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(DeleteTemplateResponse)
-	err := c.cc.Invoke(ctx, Log_DeleteTemplate_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, Mailer_DeleteTemplate_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// LogServer is the server API for Log service.
-// All implementations must embed UnimplementedLogServer
+// MailerServer is the server API for Mailer service.
+// All implementations must embed UnimplementedMailerServer
 // for forward compatibility.
-type LogServer interface {
+type MailerServer interface {
 	Add(context.Context, *CreateMailerRequest) (*CreateMailerResponse, error)
 	Get(context.Context, *GetMailerRequest) (*GetMailerResponse, error)
 	AddTemplate(context.Context, *CreateTemplateRequest) (*CreateTemplateResponse, error)
 	GetTemplate(context.Context, *GetTemplateRequest) (*GetTemplateResponse, error)
 	UpdateTemplate(context.Context, *UpdateTemplateRequest) (*UpdateTemplateResponse, error)
 	DeleteTemplate(context.Context, *DeleteTemplateRequest) (*DeleteTemplateResponse, error)
-	mustEmbedUnimplementedLogServer()
+	mustEmbedUnimplementedMailerServer()
 }
 
-// UnimplementedLogServer must be embedded to have
+// UnimplementedMailerServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedLogServer struct{}
+type UnimplementedMailerServer struct{}
 
-func (UnimplementedLogServer) Add(context.Context, *CreateMailerRequest) (*CreateMailerResponse, error) {
+func (UnimplementedMailerServer) Add(context.Context, *CreateMailerRequest) (*CreateMailerResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Add not implemented")
 }
-func (UnimplementedLogServer) Get(context.Context, *GetMailerRequest) (*GetMailerResponse, error) {
+func (UnimplementedMailerServer) Get(context.Context, *GetMailerRequest) (*GetMailerResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
 }
-func (UnimplementedLogServer) AddTemplate(context.Context, *CreateTemplateRequest) (*CreateTemplateResponse, error) {
+func (UnimplementedMailerServer) AddTemplate(context.Context, *CreateTemplateRequest) (*CreateTemplateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddTemplate not implemented")
 }
-func (UnimplementedLogServer) GetTemplate(context.Context, *GetTemplateRequest) (*GetTemplateResponse, error) {
+func (UnimplementedMailerServer) GetTemplate(context.Context, *GetTemplateRequest) (*GetTemplateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetTemplate not implemented")
 }
-func (UnimplementedLogServer) UpdateTemplate(context.Context, *UpdateTemplateRequest) (*UpdateTemplateResponse, error) {
+func (UnimplementedMailerServer) UpdateTemplate(context.Context, *UpdateTemplateRequest) (*UpdateTemplateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateTemplate not implemented")
 }
-func (UnimplementedLogServer) DeleteTemplate(context.Context, *DeleteTemplateRequest) (*DeleteTemplateResponse, error) {
+func (UnimplementedMailerServer) DeleteTemplate(context.Context, *DeleteTemplateRequest) (*DeleteTemplateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteTemplate not implemented")
 }
-func (UnimplementedLogServer) mustEmbedUnimplementedLogServer() {}
-func (UnimplementedLogServer) testEmbeddedByValue()             {}
+func (UnimplementedMailerServer) mustEmbedUnimplementedMailerServer() {}
+func (UnimplementedMailerServer) testEmbeddedByValue()                {}
 
-// UnsafeLogServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to LogServer will
+// UnsafeMailerServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to MailerServer will
 // result in compilation errors.
-type UnsafeLogServer interface {
-	mustEmbedUnimplementedLogServer()
+type UnsafeMailerServer interface {
+	mustEmbedUnimplementedMailerServer()
 }
 
-func RegisterLogServer(s grpc.ServiceRegistrar, srv LogServer) {
-	// If the following call pancis, it indicates UnimplementedLogServer was
+func RegisterMailerServer(s grpc.ServiceRegistrar, srv MailerServer) {
+	// If the following call pancis, it indicates UnimplementedMailerServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&Log_ServiceDesc, srv)
+	s.RegisterService(&Mailer_ServiceDesc, srv)
 }
 
-func _Log_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateMailerRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).Add(ctx, in)
+		return srv.(MailerServer).Add(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_Add_FullMethodName,
+		FullMethod: Mailer_Add_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).Add(ctx, req.(*CreateMailerRequest))
+		return srv.(MailerServer).Add(ctx, req.(*CreateMailerRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Log_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetMailerRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).Get(ctx, in)
+		return srv.(MailerServer).Get(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_Get_FullMethodName,
+		FullMethod: Mailer_Get_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).Get(ctx, req.(*GetMailerRequest))
+		return srv.(MailerServer).Get(ctx, req.(*GetMailerRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Log_AddTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_AddTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateTemplateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).AddTemplate(ctx, in)
+		return srv.(MailerServer).AddTemplate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_AddTemplate_FullMethodName,
+		FullMethod: Mailer_AddTemplate_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).AddTemplate(ctx, req.(*CreateTemplateRequest))
+		return srv.(MailerServer).AddTemplate(ctx, req.(*CreateTemplateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Log_GetTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_GetTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetTemplateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).GetTemplate(ctx, in)
+		return srv.(MailerServer).GetTemplate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_GetTemplate_FullMethodName,
+		FullMethod: Mailer_GetTemplate_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).GetTemplate(ctx, req.(*GetTemplateRequest))
+		return srv.(MailerServer).GetTemplate(ctx, req.(*GetTemplateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Log_UpdateTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_UpdateTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UpdateTemplateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).UpdateTemplate(ctx, in)
+		return srv.(MailerServer).UpdateTemplate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_UpdateTemplate_FullMethodName,
+		FullMethod: Mailer_UpdateTemplate_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).UpdateTemplate(ctx, req.(*UpdateTemplateRequest))
+		return srv.(MailerServer).UpdateTemplate(ctx, req.(*UpdateTemplateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Log_DeleteTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Mailer_DeleteTemplate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(DeleteTemplateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogServer).DeleteTemplate(ctx, in)
+		return srv.(MailerServer).DeleteTemplate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Log_DeleteTemplate_FullMethodName,
+		FullMethod: Mailer_DeleteTemplate_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogServer).DeleteTemplate(ctx, req.(*DeleteTemplateRequest))
+		return srv.(MailerServer).DeleteTemplate(ctx, req.(*DeleteTemplateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Log_ServiceDesc is the grpc.ServiceDesc for Log service.
+// Mailer_ServiceDesc is the grpc.ServiceDesc for Mailer service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Log_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "Log",
-	HandlerType: (*LogServer)(nil),
+var Mailer_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "Mailer",
+	HandlerType: (*MailerServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Add",
-			Handler:    _Log_Add_Handler,
+			Handler:    _Mailer_Add_Handler,
 		},
 		{
 			MethodName: "Get",
-			Handler:    _Log_Get_Handler,
+			Handler:    _Mailer_Get_Handler,
 		},
 		{
 			MethodName: "AddTemplate",
-			Handler:    _Log_AddTemplate_Handler,
+			Handler:    _Mailer_AddTemplate_Handler,
 		},
 		{
 			MethodName: "GetTemplate",
-			Handler:    _Log_GetTemplate_Handler,
+			Handler:    _Mailer_GetTemplate_Handler,
 		},
 		{
 			MethodName: "UpdateTemplate",
-			Handler:    _Log_UpdateTemplate_Handler,
+			Handler:    _Mailer_UpdateTemplate_Handler,
 		},
 		{
 			MethodName: "DeleteTemplate",
-			Handler:    _Log_DeleteTemplate_Handler,
+			Handler:    _Mailer_DeleteTemplate_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
